37519205a51d804401eb4ac6de8cd8058101312a
Holger Erik Christiansen <holg1525@edu.eal.dk>
2019-11-19 10:32:51 +0100

    validerings forslag

--- GeneraleClasser/validation.cs
+++ GeneraleClasser/validation.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace GeneraleClasser
 {
-    public class validation
+    public static class Validation
     {
+        //tjekker om der kun er 0-9 i en string
+
+
+
+
+        //tjekker om der kun er a-å, umlaut
+        //tjekker om det er en gylding adresse (0-9 og a-å, komma, umlaut);
+        //tjekker om en string har en max længe
+        //tjekker om en string har en min længe
+        //tjekker om en string er tom
+        //tjekker om en tal har et max størrelse
+        //tjekker om en tal har en min størrelse 
+        
     }
 }
