1df7817f07b30e325130f5f0c9c2a622a90214b4
Radwan Nabeel Alfakseh <radw0040@edu.eal.dk>
2019-12-03 09:40:51 +0100

    MailSystem

--- AnimaleHouseModel/EmailSystem.cs
+++ AnimaleHouseModel/EmailSystem.cs
@@ -1,22 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using AnimalHouse_Entities;
+﻿using System.Collections.Generic;
 using System.Threading;
+
 namespace AnimaleHouseModel
 {
     public class EmailSystem
     {
         public List<AnimalHouse_Entities.Email> EmailList = new List<AnimalHouse_Entities.Email>();
-        public EmailSystem(){}
+        public EmailSystem() { }
         public void CreateEmail()
         {
             foreach (AnimalHouse_Entities.Email email in EmailList)
             {
-                Thread t = new Thread(new ThreadStart(SendEmail(email)));
-                t.Start();       
+                Thread t = new Thread(() => SendEmail(email));
+                t.Start();
             }
         }
         private void SendEmail(AnimalHouse_Entities.Email e)
