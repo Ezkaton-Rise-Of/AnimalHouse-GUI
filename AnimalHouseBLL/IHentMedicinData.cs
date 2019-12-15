5f2c5bae71d2d8e299ac21f83a7d7cd6f2af49a9
Holger Erik Christiansen <holg1525@edu.eal.dk>
2019-12-15 17:37:57 +0100

    kok

---
+++ AnimalHouseBLL/IHentMedicinData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalHouseBLL
+{
+    public interface IHentMedicinData
+    {
+        bool UpdaterMedicin();
+        string Navn
+        {
+            set;
+        }        
+        string Beskrivelse
+        {
+            set;
+        }
+        string  
+
+    }
+}
